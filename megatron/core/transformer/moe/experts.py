fae6619c96654740b171c8495b2da6180db81bdc
Qi Zhang <qizhang@nvidia.com>
2026-01-22 01:50:21 -0800

    Use non blocking h2d for tokens_per_expert

--- megatron/core/transformer/moe/experts.py
+++ megatron/core/transformer/moe/experts.py
@@ -861,10 +861,12 @@ class TEGroupedMLP(MegatronModule):
             output (torch.Tensor): The output of the local experts.
         """
         if self.config.moe_use_device_initiated_grouped_gemm:
-            if not(tokens_per_expert.is_cuda or tokens_per_expert.is_pinned()):
-                tokens_per_expert = tokens_per_expert.long().cuda()
-            else:
-                tokens_per_expert = tokens_per_expert.long()
+            # TODO: Fix cutlass kenrel bug for pinned memory and remove this WORKAROUND.
+            tokens_per_expert = tokens_per_expert.long().to("cuda", non_blocking=True)
+            # if not(tokens_per_expert.is_cuda or tokens_per_expert.is_pinned()):
+            #     tokens_per_expert = tokens_per_expert.long().cuda()
+            # else:
+            #     tokens_per_expert = tokens_per_expert.long()
         else:
             tokens_per_expert = tokens_per_expert.long().cpu().tolist()
         
